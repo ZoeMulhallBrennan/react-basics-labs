fbe9b65699430489888aba55002b55ed3709c3f7
zbrennan5642 <zbrennan5642@gmail.com>
2025-09-22 16:51:52 +0100

    Delete a task

--- tasky/src/App.jsx
+++ tasky/src/App.jsx
@@ -18,6 +18,13 @@ function App() {
     setTaskState({tasks});
   }
 
+    const deleteHandler = (taskIndex) => {
+    const tasks = [...taskState.tasks];
+    tasks.splice(taskIndex, 1);
+    setTaskState({tasks});
+  } 
+
+
 
   return (
     <div className="container">
@@ -30,6 +37,8 @@ function App() {
       key={task.id}
       done={task.done}
       markDone={() => doneHandler(index)}
+      deleteTask = {() => deleteHandler(index)}
+
     />
   ))}
 
